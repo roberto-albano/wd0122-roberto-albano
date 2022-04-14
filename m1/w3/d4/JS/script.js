b0efb38abb30b1f3f9c1c925b0845ca64b8fc0cc
roberto-albano <robalb91@gmail.com>
2022-04-14 15:30:58 +0200

    Esercizi

---
+++ m1/w3/d4/JS/script.js
@@ -0,0 +1,61 @@
+// String min-max // 
+
+let text = "Salve salvino!";
+let min = text.toLowerCase();
+
+console.log(min);
+
+let max = text.toUpperCase();
+
+console.log(max);
+
+// String split + concat //
+
+let text2 = "Salve salvino!"
+let split = text.split("");
+console.log(split);
+console.log(split[0]);
+
+//concat//
+
+let text3 = "Buon";
+let text4 = "giorno!";
+let result = text3.concat(text4);
+console.log(result);
+
+//Array//
+
+let x = ['Mario' , 'Giovanni' , 'Francesco' , 'Roberto']
+console.log(x[2]);
+
+//Push//
+
+let frutti = ["Banana", "Arancia", "Mela", "Mango"];
+frutti.push("Kiwi");
+console.log(frutti);
+
+//pop//
+
+let frutti2 = ["Banana", "Arancia", "Mela", "Mango"];
+frutti2.pop();
+console.log(frutti2);
+
+//shift//
+
+let frutti3 = ["Banana", "Orange", "Apple", "Mango"];
+frutti3.shift();
+console.log(frutti3);
+
+//unshift//
+
+let frutti4 = ["Banana", "Orange", "Apple", "Mango"];
+frutti4.unshift("Limone" , "Ananas");
+console.log(frutti4);
+
+//Funzione//
+
+function calcArr(){
+    let arrayNum = [16, 25, 86];
+    arrayNum.push(77)
+    return arrayNum.length;
+}
